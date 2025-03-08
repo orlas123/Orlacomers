3a9254de5c11f9ce1df9680486c3dd1b97cf329f
Orlacomer <162639930+orlas123@users.noreply.github.com>
2025-03-08 14:21:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -219,13 +219,7 @@ if ('serviceWorker' in navigator) {
     });
 
      
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        console.log('Service Worker registrado com sucesso:', registration.scope);
-      }).catch(function(error) {
-        console.log('Falha ao registrar o Service Worker:', error);
-      });
-    }
+   
   
 
     // Lógica para instalação
