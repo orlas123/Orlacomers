b46d0924a389865351e9516c95d43ffdba65b0ac
Orlacomer <162639930+orlas123@users.noreply.github.com>
2025-03-12 22:55:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -210,14 +210,14 @@ window.onclick = function(event) {
 
 // Registro do Service Worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registrado com sucesso:', registration);
-      })
-      .catch((error) => {
-        console.log('Falha ao registrar o Service Worker:', error);
-      });
-  }
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js').then((registration) => {
+            console.log('Service Worker registrado com sucesso:', registration);
+        }).catch((error) => {
+            console.log('Falha ao registrar o Service Worker:', error);
+        });
+    });
+}
   
 
 
