0820746f3b547ed1f1545efbc2b1977a8fcf24ef
Orlacomer <162639930+orlas123@users.noreply.github.com>
2025-03-16 21:49:33 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,16 +11,49 @@ const urlsToCache = [
 
 // Instalação do Service Worker
 self.addEventListener('install', (event) => {
+  console.log('Service Worker: Instalando...');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Service Worker: Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.error('Service Worker: Falha ao abrir o cache', error);
+      })
+  );
+});
+
+// Ativação do Service Worker
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Ativando...');
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Service Worker: Deletando cache antigo', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
 // Interceptar solicitações de rede
 self.addEventListener('fetch', (event) => {
+  console.log('Service Worker: Fetching', event.request.url);
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then((response) => {
+        if (response) {
+          console.log('Service Worker: Retornando do cache', event.request.url);
+          return response;
+        }
+        console.log('Service Worker: Fetch da rede', event.request.url);
+        return fetch(event.request);
+      })
   );
 });
